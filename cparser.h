310ef6dc4bc1ba3f1644ee1ba8e4ace9991da391
William Wang <williamwong0409@gmail.com>
2018-12-08 22:51:57 -0600

    forgot the header

--- cparser.h
+++ cparser.h
@@ -4,6 +4,11 @@
 
 #ifndef C_CODE_HOT_SWAP_CPARSER_H
 #define C_CODE_HOT_SWAP_CPARSER_H
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 int isolateFunction(const char* inFile, const char * funcName, const char* writeFile);
 
